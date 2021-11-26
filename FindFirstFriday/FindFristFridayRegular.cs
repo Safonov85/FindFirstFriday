26f7909516b58e943c417970d2e956512a0987cc
safonov <safonov1985@protonmail.com>
2021-11-26 19:18:08 +0100

    Cleaned up the code, only first friday focused

--- FindFirstFriday/FindFristFridayRegular.cs
+++ FindFirstFriday/FindFristFridayRegular.cs
@@ -10,8 +10,6 @@ namespace FindFirstFriday
     public class FindFristFridayRegular
     {
         public List<string> allFridaysList = new List<string>();
-        public List<string> allFridaysLINQlist = new List<string>();
-        public List<string> numberNamesList = new List<string>();
 
 
         // Regular
@@ -26,184 +24,14 @@ namespace FindFirstFriday
                 {
                     // new date days
 
-                    DateTime timeDate = new DateTime(year, j, i);
-                    if (timeDate.ToString("dddd") == "Friday")
+                    DateTime dateTime = new DateTime(year, j, i);
+                    if (dateTime.ToString("dddd") == "Friday")
                     {
-
-                        //Debug.WriteLine(timeDate.ToString("MMMM") + " " + timeDate.Day);
-                        allFridaysList.Add(timeDate.ToString("MMMM") + " " + timeDate.Day);
+                        allFridaysList.Add(dateTime.ToString("MMMM") + " " + dateTime.Day);
                         break;
                     }
                 }
             }
-        }
-
-
-        // LINQ 
-        public void CreateFirstFridayLINQ()
-        {
-            // Find First friday in a month using LINQ
-            //DateTime firstSunday = new DateTime(2021, 6, 7);
-            //var bookings = from b in this.db.Bookings
-            //               where b.StartDateTime.DayOfWeek == DayOfWeek.Friday
-            //               select b;
-
-            var numbers = new List<int> { 1, 8, 3, 2, 5 };
-            int maxNumber = numbers.Last();
-
-            allFridaysLINQlist.Add(maxNumber.ToString());
-        }
-
-        public void CreateFirstFridayLINQTest()
-        {
-            // Testing with LINQ
-            string[] nums = { "One",
-                              "Two",
-                              "Three",
-                              "Four",
-                              "Five",
-                              "Six",
-                              "Seven",
-                              "Eight",
-                              "Nine",
-                              "Ten",
-                              "Eleven",
-                              "Twelve",
-                              "Thirteen",
-                              "Fourteen",
-                              "Fifteen",
-                              "Sixteen",
-                              "Seventeen"
-                           };
-            //Linq query  
-            IEnumerable<string> numberNames = from i in nums
-                                              where i.StartsWith("F")
-                                                select i;
-            foreach (var num in numberNames)
-            {
-                numberNamesList.Add(num);
-            }
-
-        }
-
-        public void CreateFirstFridayLINQTest2()
-        {
-            // Testing with LINQ
-            string[] nums = { "One",
-                              "Two",
-                              "Three",
-                              "Four",
-                              "Five",
-                              "Six",
-                              "Seven",
-                              "Eight",
-                              "Nine",
-                              "Ten",
-                              "Eleven",
-                              "Twelve",
-                              "Thirteen",
-                              "Fourteen",
-                              "Fifteen",
-                              "Sixteen",
-                              "Seventeen"
-                           };
-
-            var result = nums.Skip(5);
-            numberNamesList.Clear();
-            foreach (var num in result)
-            {
-                numberNamesList.Add(num);
-            }
-
-        }
-
-        public void CreateFirstFridayLINQTest3()
-        {
-            // Testing with LINQ
-            int[] nums = {    78,
-                              432,
-                              4,
-                              12,
-                              54,
-                              543,
-                              7,
-                              1,
-                              83,
-                              92,
-                              23,
-                              23,
-                              14,
-                           };
-
-            var result = nums.Average();
-            numberNamesList.Clear();
-
-            numberNamesList.Add(result.ToString());
-
-        }
-
-        
-        public void CreateFirstFridayLINQTest4()
-        {
-            // Testing with LINQ
-            int[] nums = {    78,
-                              432,
-                              4,
-                              12,
-                              54,
-                              543,
-                              7,
-                              1,
-                              83,
-                              92,
-                              23,
-                              23,
-                              14,
-                           };
-
-            // Query LINQ
-            var res = from n in nums
-                      where n > 60
-                      orderby n
-                      select n.ToString();
-
-            numberNamesList.Clear();
-
-            // Lambda LINQ
-            var rus = nums.Where(n => n > 60)
-                          .OrderBy(n => n)
-                          .Select(n => n.ToString());
-
-            // Both give SAME result
-
-            foreach (var i in res)
-            {
-                numberNamesList.Add(i);
-            }
-
-        }
-
-        public void CreateFirstFridayLINQTest5()
-        {
-            numberNamesList.Clear();
-
-            string[] nums = {
-                              "Two",
-                              "Three",
-                              "Ten",
-                              "Twelve",
-                              "Thirteen",
-                              "Million",
-                              "Thirty"
-                           };
-
-            
-            var res = nums.All(n => n.StartsWith("T")); // Does all start with letter T ?
-
-            
-            numberNamesList.Add(res.ToString()); // result
-            
-
         }
     }
 }
--- FindFirstFriday/Form1.cs
+++ FindFirstFriday/Form1.cs
@@ -19,29 +19,11 @@ namespace FindFirstFriday
             InitializeComponent();
 
             findRegular.CreateFirstFriDays();
-            findRegular.CreateFirstFridayLINQ();
-            findRegular.CreateFirstFridayLINQTest();
-            findRegular.CreateFirstFridayLINQTest2();
-            findRegular.CreateFirstFridayLINQTest3();
-            findRegular.CreateFirstFridayLINQTest4();
-            findRegular.CreateFirstFridayLINQTest5();
 
             foreach (var item in findRegular.allFridaysList)
             {
                 RegularFindListBox.Items.Add(item);
             }
-
-            foreach (var item in findRegular.numberNamesList)
-            {
-                //LINQlistBox.Items.Add(item);
-                LINQlistBox.Items.Add(item);
-            }
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
